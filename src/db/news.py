9a179e17bc7a53651d10b3d111b8ca964e6b3583
B1t0ne <hornid2010@mail.ru>
2026-07-03 15:43:15 +0300

    some updates, parser, and new data

--- src/db/news.py
+++ src/db/news.py
@@ -13,15 +13,15 @@ class News(SqlAlchemyBase, JsonSerializableMixin):
     id = sqlalchemy.Column(
         sqlalchemy.String, primary_key=True, unique=True, nullable=False, default=lambda: uuid.uuid4().hex)
     project_id = sqlalchemy.Column(
-        sqlalchemy.String, sqlalchemy.ForeignKey("projects.id", ondelete="CASCADE"), nullable=False, index=True)
-    project_name = sqlalchemy.Column(sqlalchemy.String, nullable=False)
-    developer = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+        sqlalchemy.String, sqlalchemy.ForeignKey("projects.id", ondelete="CASCADE"), nullable=True, index=True)
+    project_name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    developer = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     title = sqlalchemy.Column(sqlalchemy.String, nullable=False)
     content = sqlalchemy.Column(sqlalchemy.Text, nullable=False)
-    date = sqlalchemy.Column(sqlalchemy.Date, nullable=False)
+    date = sqlalchemy.Column(sqlalchemy.Date, default=datetime.datetime.now, nullable=False)
     source = sqlalchemy.Column(sqlalchemy.String, nullable=False)
-    category = sqlalchemy.Column(sqlalchemy.String, nullable=False)
-    sentiment = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    category = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    sentiment = sqlalchemy.Column(sqlalchemy.String, default="neutral", nullable=False)
     created_at = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.now, nullable=False)
 
     project = orm.relationship("Project", back_populates="news")
